bebe3711c89e6ee0f32f96422363336f71212c0d
sh5080 <prpn97@gmail.com>
2024-12-19 16:37:57 +0900

    feat: settings

--- src/web/next.config.mjs
+++ src/web/next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL,
+    NEXT_PUBLIC_FRONTEND_PORT: process.env.NEXT_PUBLIC_FRONTEND_PORT,
+    NEXT_PUBLIC_SERVER_PORT: process.env.NEXT_PUBLIC_SERVER_PORT,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `http://localhost:${process.env.NEXT_PUBLIC_SERVER_PORT}/api/:path*`,
+      },
+    ];
+  },
+};
+
+console.log('환경변수 확인:', {
+  FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL,
+  FRONTEND_PORT: process.env.NEXT_PUBLIC_FRONTEND_PORT,
+  SERVER_PORT: process.env.NEXT_PUBLIC_SERVER_PORT,
+});
 
 export default nextConfig;
