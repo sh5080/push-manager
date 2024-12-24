25bef34577fcd327bd439829bd8148bb29780749
sh5080 <prpn97@gmail.com>
2024-12-24 15:23:04 +0900

    feat: queue repo init

--- src/server/src/routes/push.route.ts
+++ src/server/src/routes/push.route.ts
@@ -3,11 +3,13 @@ import { PushController } from "../controllers/push.controller";
 import { PushService } from "../services/push.service";
 import { PushStsMsgRepository } from "../repositories/pushStsMsg.repository";
 import { PushMasterRepository } from "../repositories/pushMaster.repository";
+import { PushQueueRepository } from "../repositories/pushQueue.repository";
 
 const router = Router();
 const pushService = new PushService(
   new PushMasterRepository(),
-  new PushStsMsgRepository()
+  new PushStsMsgRepository(),
+  new PushQueueRepository()
 );
 const pushController = new PushController(pushService);
 
