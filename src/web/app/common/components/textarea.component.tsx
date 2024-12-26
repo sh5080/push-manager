20e4a5bd409be1342451759c1db7c93ad0599306
sh5080 <prpn97@gmail.com>
2024-12-26 17:47:15 +0900

    feat: 식별자 렌더링

--- src/web/app/common/components/textarea.component.tsx
+++ src/web/app/common/components/textarea.component.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from "react";
 import { Textarea } from "@headlessui/react";
 
 interface TextareaProps
@@ -6,28 +7,28 @@ interface TextareaProps
   label?: string;
 }
 
-export function TextareaComponent({
-  rows = 3,
-  label,
-  className,
-  ...props
-}: TextareaProps) {
-  return (
-    <div>
-      {label && (
-        <label className="block text-sm font-medium text-gray-700 mb-1">
-          {label}
-        </label>
-      )}
-      <Textarea
-        rows={rows}
-        className={`
+export const TextareaComponent = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ rows = 3, label, className, ...props }, ref) => {
+    return (
+      <div>
+        {label && (
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            {label}
+          </label>
+        )}
+        <Textarea
+          ref={ref}
+          rows={rows}
+          className={`
           mt-3 block w-full resize-none rounded-lg border-none bg-gray-50 py-1.5 px-3 text-sm/6 text-gray-700
           focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-gray-200
           ${className || ""}
         `}
-        {...props}
-      />
-    </div>
-  );
-}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
+
+TextareaComponent.displayName = "TextareaComponent";
