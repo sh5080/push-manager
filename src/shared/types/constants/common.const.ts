313aecbad09fbc6c1938e63d468e4388730e3c6a
sh5080 <prpn97@gmail.com>
2024-12-27 14:21:29 +0900

    feat: identify 관련 const

--- src/shared/types/constants/common.const.ts
+++ src/shared/types/constants/common.const.ts
@@ -1,10 +1,21 @@
-export const AppIdEnum = {
-  FREED: "0",
-  TEST: "1",
-  PROD: "2",
-} as const;
+import { envConfig } from "@push-manager/shared/configs/env.config";
 
 export const PushTypeEnum = {
-  TARGET: "0",
-  ALL: "1",
+  TARGET: 0,
+  ALL: 1,
 } as const;
+
+export const AppIdEnum = {
+  FREED: 0,
+  TEST: 1,
+  PROD: 2,
+} as const;
+
+export const TeamIdEnum = {
+  FREED: 0,
+  LG: 1,
+} as const;
+
+const { keyFreed, keyFreed2, keyPrd } = envConfig.push;
+
+export const APP_ID_LIST = [keyFreed, keyFreed2, keyPrd];
