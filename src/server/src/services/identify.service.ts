02deb172a0d6c142b8bd36025c80fed83cd87461
sh5080 <prpn97@gmail.com>
2024-12-27 17:35:19 +0900

    fix: findAll 조건별 처리

--- src/server/src/services/identify.service.ts
+++ src/server/src/services/identify.service.ts
@@ -11,8 +11,39 @@ export class IdentifyService implements IIdentifyService {
   constructor(private readonly identifyRepository: IdentifyRepository) {}
 
   async getIdentifies(dto: GetIdentifiesDto) {
+    // AppId 처리
+    let appIds: number[] = [];
+    if (dto.appId) {
+      if (dto.appId === 3) {
+        // 전체
+        appIds = [1, 2, 3];
+      } else if (dto.appId === 1) {
+        // 테스트 환경
+        appIds = [1, 3];
+      } else if (dto.appId === 2) {
+        // 운영 환경
+        appIds = [2, 3];
+      }
+    }
+
+    // TeamId 처리
+    let teamIds: number[] = [];
+    if (dto.teamId) {
+      if (dto.teamId === 3) {
+        // 전체
+        teamIds = [1, 2];
+      } else if (dto.teamId === 1) {
+        // FREED
+        teamIds = [1];
+      } else if (dto.teamId === 2) {
+        // LG
+        teamIds = [2];
+      }
+    }
+
     return queryRunnerCreation(
-      (queryRunner) => this.identifyRepository.findAll(queryRunner, dto),
+      (queryRunner) =>
+        this.identifyRepository.findAll(queryRunner, teamIds, appIds),
       false
     );
   }
@@ -20,7 +51,7 @@ export class IdentifyService implements IIdentifyService {
   async createIdentify(dto: CreateIdentifyDto) {
     return queryRunnerCreation(async (queryRunner) => {
       const data = await this.identifyRepository.create(queryRunner, dto);
-      return data.IDX;
+      return data.idx;
     });
   }
 
