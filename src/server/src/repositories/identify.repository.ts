549941bea98f52889d5bf1d8df8cf7b88052a435
sh5080 <prpn97@gmail.com>
2024-12-27 17:35:19 +0900

    fix: findAll 조건별 처리

--- src/server/src/repositories/identify.repository.ts
+++ src/server/src/repositories/identify.repository.ts
@@ -1,4 +1,4 @@
-import { QueryRunner, Repository } from "typeorm";
+import { QueryRunner } from "typeorm";
 import { TestIdentify } from "../entities/testIdentify.entity";
 import {
   CreateIdentifyDto,
@@ -13,36 +13,40 @@ export class IdentifyRepository extends BaseRepository<TestIdentify> {
     super(AppDataSource, TestIdentify);
   }
 
-  async findAll(queryRunner: QueryRunner, dto: GetIdentifiesDto) {
+  async findAll(
+    queryRunner: QueryRunner,
+    teamIds?: number[],
+    appIds?: number[]
+  ) {
     const query = queryRunner.manager
       .getRepository(TestIdentify)
       .createQueryBuilder("identify");
 
-    if (dto.teamId) {
-      query.andWhere("identify.teamId = :teamId", { teamId: dto.teamId });
+    if (teamIds && teamIds.length > 0) {
+      query.andWhere("identify.TEAMID IN (:...teamIds)", { teamIds });
     }
 
-    if (dto.appId) {
-      query.andWhere("identify.appId = :appId", { appId: dto.appId });
+    if (appIds && appIds.length > 0) {
+      query.andWhere("identify.APPID IN (:...appIds)", { appIds });
     }
 
-    return query.orderBy("identify.IDX", "DESC").getMany();
+    return query.orderBy("identify.IDX", "ASC").getMany();
   }
 
   async create(queryRunner: QueryRunner, dto: CreateIdentifyDto) {
     const identify = new TestIdentify();
-    identify.APPID = dto.appId;
+    identify.appId = dto.appId;
     return queryRunner.manager.save(identify);
   }
 
   async update(queryRunner: QueryRunner, dto: UpdateIdentifyDto) {
     const identify = await queryRunner.manager.findOne(TestIdentify, {
-      where: { IDX: dto.idx },
+      where: { idx: dto.idx },
     });
     if (!identify) {
       throw new Error("Identify not found");
     }
-    identify.IDENTIFY = dto.identify;
+    identify.identify = dto.identify;
     return queryRunner.manager.save(identify);
   }
 
