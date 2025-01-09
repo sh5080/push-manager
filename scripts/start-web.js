2fc8885b0527c6a5ef56d9d7088c232f5723395a
sh5080 <prpn97@gmail.com>
2025-01-09 16:07:24 +0900

    fix: 내부망 호스트 start-web.js에서실행

--- scripts/start-web.js
+++ scripts/start-web.js
@@ -1,10 +1,22 @@
 const exec = require("child_process").exec;
 const path = require("path");
+const { getNetworkIP } = require("./set-env");
 
-const client = exec("yarn ws:web start", {
-  windowsHide: true,
-  cwd: path.join(__dirname, "../"),
-});
+const ip = getNetworkIP();
+console.log(`Starting Next.js server on ${ip}:8888`);
+
+const client = exec(
+  `cd "${path.join(__dirname, "../src/web")}" && next start -H ${ip} -p 8888`,
+  {
+    windowsHide: true,
+    cwd: path.join(__dirname, "../"),
+  }
+);
 
 client.stdout.pipe(process.stdout);
 client.stderr.pipe(process.stderr);
+
+client.on("error", (error) => {
+  console.error("Failed to start web server:", error);
+  process.exit(1);
+});
