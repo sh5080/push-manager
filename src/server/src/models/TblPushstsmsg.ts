1b5546628913396db856c8172a919a836a5ea066
sh5080 <prpn97@gmail.com>
2025-01-13 16:32:00 +0900

    fix: idx(msg_idx) DataTypes.BIGINT로 수정 (js number 타입한계로 정확도 떨어져 다르게 반환될 때가 있음), 해결함

--- src/server/src/models/TblPushstsmsg.ts
+++ src/server/src/models/TblPushstsmsg.ts
@@ -1,5 +1,5 @@
-import * as Sequelize from 'sequelize';
-import { DataTypes, Model, Optional } from 'sequelize';
+import * as Sequelize from "sequelize";
+import { DataTypes, Model, Optional } from "sequelize";
 
 export interface TblPushstsmsgAttributes {
   androidBadge?: any;
@@ -54,10 +54,61 @@ export interface TblPushstsmsgAttributes {
 
 export type TblPushstsmsgPk = "idx";
 export type TblPushstsmsgId = TblPushstsmsg[TblPushstsmsgPk];
-export type TblPushstsmsgOptionalAttributes = "androidBadge" | "androidSound" | "andErrormessage" | "andPriority" | "andSendCount" | "appid" | "bgcolor" | "crondate" | "cronidx" | "cronComplatedate" | "customKey1" | "customKey2" | "customKey3" | "customValue1" | "customValue2" | "customValue3" | "errormessage" | "etiquetteEtime" | "etiquetteStime" | "fname" | "fontcolor" | "idlistFlag" | "iosBadge" | "iosErrormessage" | "iosSendCount" | "iosSound" | "isandroid" | "isbulk" | "isetiquette" | "isios" | "labelCode" | "link" | "ofbTime" | "optagree" | "oMode" | "resultdate" | "retry" | "senddate" | "sendspeed" | "sendStat" | "step" | "targetFilter" | "title" | "tmpMessage" | "userId";
-export type TblPushstsmsgCreationAttributes = Optional<TblPushstsmsgAttributes, TblPushstsmsgOptionalAttributes>;
+export type TblPushstsmsgOptionalAttributes =
+  | "androidBadge"
+  | "androidSound"
+  | "andErrormessage"
+  | "andPriority"
+  | "andSendCount"
+  | "appid"
+  | "bgcolor"
+  | "crondate"
+  | "cronidx"
+  | "cronComplatedate"
+  | "customKey1"
+  | "customKey2"
+  | "customKey3"
+  | "customValue1"
+  | "customValue2"
+  | "customValue3"
+  | "errormessage"
+  | "etiquetteEtime"
+  | "etiquetteStime"
+  | "fname"
+  | "fontcolor"
+  | "idlistFlag"
+  | "iosBadge"
+  | "iosErrormessage"
+  | "iosSendCount"
+  | "iosSound"
+  | "isandroid"
+  | "isbulk"
+  | "isetiquette"
+  | "isios"
+  | "labelCode"
+  | "link"
+  | "ofbTime"
+  | "optagree"
+  | "oMode"
+  | "resultdate"
+  | "retry"
+  | "senddate"
+  | "sendspeed"
+  | "sendStat"
+  | "step"
+  | "targetFilter"
+  | "title"
+  | "tmpMessage"
+  | "userId";
+export type TblPushstsmsgCreationAttributes = Optional<
+  TblPushstsmsgAttributes,
+  TblPushstsmsgOptionalAttributes
+>;
 
-export class TblPushstsmsg extends Model<TblPushstsmsgAttributes, TblPushstsmsgCreationAttributes> implements TblPushstsmsgAttributes {
+export class TblPushstsmsg
+  extends Model<TblPushstsmsgAttributes, TblPushstsmsgCreationAttributes>
+  implements TblPushstsmsgAttributes
+{
   androidBadge?: any;
   androidSound?: string;
   andErrormessage?: string;
@@ -107,295 +158,286 @@ export class TblPushstsmsg extends Model<TblPushstsmsgAttributes, TblPushstsmsgC
   userId?: string;
   wdate!: string;
 
-
   static initModel(sequelize: Sequelize.Sequelize): typeof TblPushstsmsg {
-    return TblPushstsmsg.init({
-    androidBadge: {
-      type: "NUMBER",
-      allowNull: true,
-      field: 'ANDROID_BADGE'
-    },
-    androidSound: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'ANDROID_SOUND'
-    },
-    andErrormessage: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'AND_ERRORMESSAGE'
-    },
-    andPriority: {
-      type: DataTypes.CHAR,
-      allowNull: true,
-      field: 'AND_PRIORITY'
-    },
-    andSendCount: {
-      type: "NUMBER",
-      allowNull: true,
-      field: 'AND_SEND_COUNT'
-    },
-    appid: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'APPID'
-    },
-    bgcolor: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'BGCOLOR'
-    },
-    crondate: {
-      type: DataTypes.DATEONLY,
-      allowNull: true,
-      field: 'CRONDATE'
-    },
-    cronidx: {
-      type: "NUMBER",
-      allowNull: true,
-      field: 'CRONIDX'
-    },
-    cronComplatedate: {
-      type: DataTypes.DATEONLY,
-      allowNull: true,
-      field: 'CRON_COMPLATEDATE'
-    },
-    customKey1: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'CUSTOM_KEY_1'
-    },
-    customKey2: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'CUSTOM_KEY_2'
-    },
-    customKey3: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'CUSTOM_KEY_3'
-    },
-    customValue1: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'CUSTOM_VALUE_1'
-    },
-    customValue2: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'CUSTOM_VALUE_2'
-    },
-    customValue3: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'CUSTOM_VALUE_3'
-    },
-    errormessage: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'ERRORMESSAGE'
-    },
-    etiquetteEtime: {
-      type: "NUMBER",
-      allowNull: true,
-      field: 'ETIQUETTE_ETIME'
-    },
-    etiquetteStime: {
-      type: "NUMBER",
-      allowNull: true,
-      field: 'ETIQUETTE_STIME'
-    },
-    fname: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'FNAME'
-    },
-    fontcolor: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'FONTCOLOR'
-    },
-    idlistFlag: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'IDLIST_FLAG'
-    },
-    idx: {
-      type: "NUMBER",
-      allowNull: false,
-      primaryKey: true,
-      field: 'IDX'
-    },
-    iosBadge: {
-      type: "NUMBER",
-      allowNull: true,
-      field: 'IOS_BADGE'
-    },
-    iosErrormessage: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'IOS_ERRORMESSAGE'
-    },
-    iosSendCount: {
-      type: "NUMBER",
-      allowNull: true,
-      field: 'IOS_SEND_COUNT'
-    },
-    iosSound: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'IOS_SOUND'
-    },
-    isandroid: {
-      type: DataTypes.CHAR,
-      allowNull: true,
-      field: 'ISANDROID'
-    },
-    isbulk: {
-      type: DataTypes.CHAR,
-      allowNull: true,
-      field: 'ISBULK'
-    },
-    isetiquette: {
-      type: DataTypes.CHAR,
-      allowNull: true,
-      field: 'ISETIQUETTE'
-    },
-    isios: {
-      type: DataTypes.CHAR,
-      allowNull: true,
-      field: 'ISIOS'
-    },
-    labelCode: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'LABEL_CODE'
-    },
-    link: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'LINK'
-    },
-    ofbTime: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'OFB_TIME'
-    },
-    optagree: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'OPTAGREE'
-    },
-    oMode: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'O_MODE'
-    },
-    resultdate: {
-      type: DataTypes.DATEONLY,
-      allowNull: true,
-      field: 'RESULTDATE'
-    },
-    retry: {
-      type: "NUMBER",
-      allowNull: true,
-      field: 'RETRY'
-    },
-    senddate: {
-      type: DataTypes.DATEONLY,
-      allowNull: true,
-      field: 'SENDDATE'
-    },
-    sendspeed: {
-      type: "NUMBER",
-      allowNull: true,
-      field: 'SENDSPEED'
-    },
-    sendStat: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'SEND_STAT'
-    },
-    step: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'STEP'
-    },
-    targetFilter: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'TARGET_FILTER'
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'TITLE'
-    },
-    tmpMessage: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'TMP_MESSAGE'
-    },
-    udate: {
-      type: DataTypes.DATEONLY,
-      allowNull: false,
-      field: 'UDATE'
-    },
-    userId: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'USER_ID'
-    },
-    wdate: {
-      type: DataTypes.DATEONLY,
-      allowNull: false,
-      field: 'WDATE'
-    }
-  }, {
-    sequelize,
-    tableName: 'TBL_PUSHSTSMSG',
-    schema: 'COKR_MBR_APP',
-    timestamps: false,
-    indexes: [
+    return TblPushstsmsg.init(
       {
-        name: "idx_pushstsmsg_sc",
-        fields: [
-          { name: "STEP" },
-          { name: "CRONDATE" },
-        ]
+        androidBadge: {
+          type: "NUMBER",
+          allowNull: true,
+          field: "ANDROID_BADGE",
+        },
+        androidSound: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "ANDROID_SOUND",
+        },
+        andErrormessage: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "AND_ERRORMESSAGE",
+        },
+        andPriority: {
+          type: DataTypes.CHAR,
+          allowNull: true,
+          field: "AND_PRIORITY",
+        },
+        andSendCount: {
+          type: "NUMBER",
+          allowNull: true,
+          field: "AND_SEND_COUNT",
+        },
+        appid: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "APPID",
+        },
+        bgcolor: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "BGCOLOR",
+        },
+        crondate: {
+          type: DataTypes.DATEONLY,
+          allowNull: true,
+          field: "CRONDATE",
+        },
+        cronidx: {
+          type: "NUMBER",
+          allowNull: true,
+          field: "CRONIDX",
+        },
+        cronComplatedate: {
+          type: DataTypes.DATEONLY,
+          allowNull: true,
+          field: "CRON_COMPLATEDATE",
+        },
+        customKey1: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "CUSTOM_KEY_1",
+        },
+        customKey2: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "CUSTOM_KEY_2",
+        },
+        customKey3: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "CUSTOM_KEY_3",
+        },
+        customValue1: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "CUSTOM_VALUE_1",
+        },
+        customValue2: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "CUSTOM_VALUE_2",
+        },
+        customValue3: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "CUSTOM_VALUE_3",
+        },
+        errormessage: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "ERRORMESSAGE",
+        },
+        etiquetteEtime: {
+          type: "NUMBER",
+          allowNull: true,
+          field: "ETIQUETTE_ETIME",
+        },
+        etiquetteStime: {
+          type: "NUMBER",
+          allowNull: true,
+          field: "ETIQUETTE_STIME",
+        },
+        fname: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "FNAME",
+        },
+        fontcolor: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "FONTCOLOR",
+        },
+        idlistFlag: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "IDLIST_FLAG",
+        },
+        idx: {
+          type: DataTypes.BIGINT,
+          allowNull: false,
+          primaryKey: true,
+          field: "IDX",
+        },
+        iosBadge: {
+          type: "NUMBER",
+          allowNull: true,
+          field: "IOS_BADGE",
+        },
+        iosErrormessage: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "IOS_ERRORMESSAGE",
+        },
+        iosSendCount: {
+          type: "NUMBER",
+          allowNull: true,
+          field: "IOS_SEND_COUNT",
+        },
+        iosSound: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "IOS_SOUND",
+        },
+        isandroid: {
+          type: DataTypes.CHAR,
+          allowNull: true,
+          field: "ISANDROID",
+        },
+        isbulk: {
+          type: DataTypes.CHAR,
+          allowNull: true,
+          field: "ISBULK",
+        },
+        isetiquette: {
+          type: DataTypes.CHAR,
+          allowNull: true,
+          field: "ISETIQUETTE",
+        },
+        isios: {
+          type: DataTypes.CHAR,
+          allowNull: true,
+          field: "ISIOS",
+        },
+        labelCode: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "LABEL_CODE",
+        },
+        link: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "LINK",
+        },
+        ofbTime: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "OFB_TIME",
+        },
+        optagree: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "OPTAGREE",
+        },
+        oMode: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "O_MODE",
+        },
+        resultdate: {
+          type: DataTypes.DATEONLY,
+          allowNull: true,
+          field: "RESULTDATE",
+        },
+        retry: {
+          type: "NUMBER",
+          allowNull: true,
+          field: "RETRY",
+        },
+        senddate: {
+          type: DataTypes.DATEONLY,
+          allowNull: true,
+          field: "SENDDATE",
+        },
+        sendspeed: {
+          type: "NUMBER",
+          allowNull: true,
+          field: "SENDSPEED",
+        },
+        sendStat: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "SEND_STAT",
+        },
+        step: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "STEP",
+        },
+        targetFilter: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "TARGET_FILTER",
+        },
+        title: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "TITLE",
+        },
+        tmpMessage: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "TMP_MESSAGE",
+        },
+        udate: {
+          type: DataTypes.DATEONLY,
+          allowNull: false,
+          field: "UDATE",
+        },
+        userId: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "USER_ID",
+        },
+        wdate: {
+          type: DataTypes.DATEONLY,
+          allowNull: false,
+          field: "WDATE",
+        },
       },
       {
-        name: "idx_tbl_pushstsmsg_asisu",
-        fields: [
-          { name: "APPID" },
-          { name: "STEP" },
-          { name: "IDLIST_FLAG" },
-          { name: "SENDDATE" },
-          { name: "USER_ID" },
-        ]
-      },
-      {
-        name: "idx_tbl_pushstsmsg_ss",
-        fields: [
-          { name: "STEP" },
-          { name: "SENDDATE" },
-        ]
-      },
-      {
-        name: "idx_tbl_pushstsmsg_us",
-        fields: [
-          { name: "USER_ID" },
-          { name: "SENDDATE" },
-        ]
-      },
-      {
-        name: "pk_tbl_pushstsmsg",
-        unique: true,
-        fields: [
-          { name: "IDX" },
-        ]
-      },
-    ]
-  });
+        sequelize,
+        tableName: "TBL_PUSHSTSMSG",
+        schema: "COKR_MBR_APP",
+        timestamps: false,
+        indexes: [
+          {
+            name: "idx_pushstsmsg_sc",
+            fields: [{ name: "STEP" }, { name: "CRONDATE" }],
+          },
+          {
+            name: "idx_tbl_pushstsmsg_asisu",
+            fields: [
+              { name: "APPID" },
+              { name: "STEP" },
+              { name: "IDLIST_FLAG" },
+              { name: "SENDDATE" },
+              { name: "USER_ID" },
+            ],
+          },
+          {
+            name: "idx_tbl_pushstsmsg_ss",
+            fields: [{ name: "STEP" }, { name: "SENDDATE" }],
+          },
+          {
+            name: "idx_tbl_pushstsmsg_us",
+            fields: [{ name: "USER_ID" }, { name: "SENDDATE" }],
+          },
+          {
+            name: "pk_tbl_pushstsmsg",
+            unique: true,
+            fields: [{ name: "IDX" }],
+          },
+        ],
+      }
+    );
   }
 }
--- src/server/src/models/TblPushstssendStatsDay.ts
+++ src/server/src/models/TblPushstssendStatsDay.ts
@@ -1,5 +1,5 @@
-import * as Sequelize from 'sequelize';
-import { DataTypes, Model, Optional } from 'sequelize';
+import * as Sequelize from "sequelize";
+import { DataTypes, Model, Optional } from "sequelize";
 
 export interface TblPushstssendStatsDayAttributes {
   appdel: any;
@@ -17,9 +17,16 @@ export interface TblPushstssendStatsDayAttributes {
   userId: string;
 }
 
-export type TblPushstssendStatsDayCreationAttributes = TblPushstssendStatsDayAttributes;
+export type TblPushstssendStatsDayCreationAttributes =
+  TblPushstssendStatsDayAttributes;
 
-export class TblPushstssendStatsDay extends Model<TblPushstssendStatsDayAttributes, TblPushstssendStatsDayCreationAttributes> implements TblPushstssendStatsDayAttributes {
+export class TblPushstssendStatsDay
+  extends Model<
+    TblPushstssendStatsDayAttributes,
+    TblPushstssendStatsDayCreationAttributes
+  >
+  implements TblPushstssendStatsDayAttributes
+{
   appdel!: any;
   appid!: string;
   deviceType!: string;
@@ -34,103 +41,102 @@ export class TblPushstssendStatsDay extends Model<TblPushstssendStatsDayAttribut
   udate!: string;
   userId!: string;
 
-
-  static initModel(sequelize: Sequelize.Sequelize): typeof TblPushstssendStatsDay {
-    return TblPushstssendStatsDay.init({
-    appdel: {
-      type: "NUMBER",
-      allowNull: false,
-      field: 'APPDEL'
-    },
-    appid: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      field: 'APPID'
-    },
-    deviceType: {
-      type: DataTypes.CHAR,
-      allowNull: false,
-      field: 'DEVICE_TYPE'
-    },
-    failed: {
-      type: "NUMBER",
-      allowNull: false,
-      field: 'FAILED'
-    },
-    msgIdx: {
-      type: "NUMBER",
-      allowNull: false,
-      field: 'MSG_IDX'
-    },
-    opened: {
-      type: "NUMBER",
-      allowNull: false,
-      field: 'OPENED'
-    },
-    resent: {
-      type: "NUMBER",
-      allowNull: false,
-      field: 'RESENT'
-    },
-    sendType: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      field: 'SEND_TYPE'
-    },
-    sent: {
-      type: "NUMBER",
-      allowNull: false,
-      field: 'SENT'
-    },
-    sms: {
-      type: "NUMBER",
-      allowNull: false,
-      field: 'SMS'
-    },
-    startd: {
-      type: DataTypes.DATEONLY,
-      allowNull: false,
-      field: 'STARTD'
-    },
-    udate: {
-      type: DataTypes.DATEONLY,
-      allowNull: false,
-      field: 'UDATE'
-    },
-    userId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      field: 'USER_ID'
-    }
-  }, {
-    sequelize,
-    tableName: 'TBL_PUSHSTSSEND_STATS_DAY',
-    schema: 'COKR_MBR_APP',
-    timestamps: false,
-    indexes: [
+  static initModel(
+    sequelize: Sequelize.Sequelize
+  ): typeof TblPushstssendStatsDay {
+    return TblPushstssendStatsDay.init(
       {
-        name: "idx_tbl_stssend_stats_day_as",
-        fields: [
-          { name: "APPID" },
-          { name: "STARTD" },
-        ]
+        appdel: {
+          type: "NUMBER",
+          allowNull: false,
+          field: "APPDEL",
+        },
+        appid: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          field: "APPID",
+        },
+        deviceType: {
+          type: DataTypes.CHAR,
+          allowNull: false,
+          field: "DEVICE_TYPE",
+        },
+        failed: {
+          type: "NUMBER",
+          allowNull: false,
+          field: "FAILED",
+        },
+        msgIdx: {
+          type: DataTypes.BIGINT,
+          allowNull: false,
+          field: "MSG_IDX",
+        },
+        opened: {
+          type: "NUMBER",
+          allowNull: false,
+          field: "OPENED",
+        },
+        resent: {
+          type: "NUMBER",
+          allowNull: false,
+          field: "RESENT",
+        },
+        sendType: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          field: "SEND_TYPE",
+        },
+        sent: {
+          type: "NUMBER",
+          allowNull: false,
+          field: "SENT",
+        },
+        sms: {
+          type: "NUMBER",
+          allowNull: false,
+          field: "SMS",
+        },
+        startd: {
+          type: DataTypes.DATEONLY,
+          allowNull: false,
+          field: "STARTD",
+        },
+        udate: {
+          type: DataTypes.DATEONLY,
+          allowNull: false,
+          field: "UDATE",
+        },
+        userId: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          field: "USER_ID",
+        },
       },
       {
-        name: "idx_tbl_stssend_stats_day_u",
-        fields: [
-          { name: "USER_ID" },
-        ]
-      },
-      {
-        name: "pk_tbl_pushstssend_stats_day",
-        unique: true,
-        fields: [
-          { name: "MSG_IDX" },
-          { name: "STARTD" },
-          { name: "DEVICE_TYPE" },
-        ]
-      },
-    ]
-  });
+        sequelize,
+        tableName: "TBL_PUSHSTSSEND_STATS_DAY",
+        schema: "COKR_MBR_APP",
+        timestamps: false,
+        indexes: [
+          {
+            name: "idx_tbl_stssend_stats_day_as",
+            fields: [{ name: "APPID" }, { name: "STARTD" }],
+          },
+          {
+            name: "idx_tbl_stssend_stats_day_u",
+            fields: [{ name: "USER_ID" }],
+          },
+          {
+            name: "pk_tbl_pushstssend_stats_day",
+            unique: true,
+            fields: [
+              { name: "MSG_IDX" },
+              { name: "STARTD" },
+              { name: "DEVICE_TYPE" },
+            ],
+          },
+        ],
+      }
+    );
   }
 }
