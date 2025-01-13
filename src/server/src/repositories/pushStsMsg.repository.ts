c5d10f12fe96c82128fd71480a5044c55277f23c
sh5080 <prpn97@gmail.com>
2025-01-13 16:51:20 +0900

    feat: appIds 생성자 안에서 관리, sequelize init된 모델로 migrate

--- src/server/src/repositories/pushStsMsg.repository.ts
+++ src/server/src/repositories/pushStsMsg.repository.ts
@@ -2,55 +2,59 @@ import { BaseRepository } from "./base.repository";
 import { PushStsMsg } from "../entities/pushStsMsg.entity";
 import { AppDataSource } from "../configs/db.config";
 import { APP_CONFIG } from "../configs/app.config";
-import { PushStsSendStatsDay } from "../entities/pushStsSendStatsDay.entity";
-import { QueryRunner } from "typeorm";
+import { TblPushstsmsg, TblPushstssendStatsDay } from "../models/init-models";
+import { Op } from "sequelize";
+import { TblPushstsmsgAlias } from "@push-manager/shared";
 
 export class PushStsMsgRepository extends BaseRepository<PushStsMsg> {
+  private appIds: string[];
+
   constructor() {
     super(AppDataSource, PushStsMsg);
-  }
-
-  async getRecentTargetPushesByAppId(
-    limit: number = 10,
-    appId: string
-  ): Promise<PushStsMsg[]> {
-    const query = `
-      SELECT * FROM (
-        SELECT * FROM TBL_PUSHSTSMSG
-        WHERE APPID = :1
-        ORDER BY IDX DESC
-      ) WHERE ROWNUM <= :2
-    `;
-
-    return this.executeRaw(query, [appId, limit]);
-  }
-
-  async getRecentTargetPushes(limit: number = 10): Promise<PushStsMsg[]> {
-    const query = `
-      SELECT * FROM (
-        SELECT * FROM TBL_PUSHSTSMSG
-        WHERE APPID IN (:1, :2, :3)
-        ORDER BY IDX DESC
-      ) WHERE ROWNUM <= :4
-    `;
-
-    const appIds = [
+    this.appIds = [
       APP_CONFIG[0].appId,
       APP_CONFIG[1].appId,
       APP_CONFIG[2].appId,
     ];
-
-    return this.executeRaw(query, [...appIds, limit]);
   }
-  async getPushStsMsgDetail(
-    queryRunner: QueryRunner,
-    idx: number
-  ): Promise<PushStsMsg | null> {
-    return queryRunner.manager
-      .getRepository(PushStsMsg)
-      .createQueryBuilder("msg")
-      .leftJoinAndSelect("msg.detail", "stats")
-      .where("msg.IDX = :idx", { idx })
-      .getOne();
+
+  async getPushStsMsgDetail(idx: string): Promise<TblPushstsmsg | null> {
+    return await TblPushstsmsg.findOne({
+      where: { idx },
+      include: [
+        {
+          model: TblPushstssendStatsDay,
+          as: TblPushstsmsgAlias.TblPushstssendStatsDay,
+          required: false,
+          where: {
+            msgIdx: idx,
+          },
+          attributes: [
+            "sent",
+            "failed",
+            "opened",
+            "appdel",
+            "sms",
+            "deviceType",
+            "startd",
+          ],
+        },
+      ],
+      raw: true,
+      nest: true,
+    });
+  }
+
+  async getRecentTargetPushes(limit: number = 10): Promise<TblPushstsmsg[]> {
+    return await TblPushstsmsg.findAll({
+      limit,
+      order: [["idx", "DESC"]],
+      raw: true,
+      where: {
+        appid: {
+          [Op.in]: this.appIds,
+        },
+      },
+    });
   }
 }
