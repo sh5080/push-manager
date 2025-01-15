944fa742b655fb28469baeb4db8d66de9707e7b9
sh5080 <prpn97@gmail.com>
2025-01-15 13:30:02 +0900

    fix: 공용타입으로 수정

--- src/server/src/interfaces/push.interface.ts
+++ src/server/src/interfaces/push.interface.ts
@@ -1,6 +1,7 @@
 import {
   CreatePushDto,
   GetRecentPushesDto,
+  IPushStsMsg,
   UpdatePushStatusDto,
 } from "@push-manager/shared";
 import { PushMaster } from "../entities/pushMaster.entity";
@@ -10,6 +11,6 @@ export interface IPushService {
   createPushes(dto: CreatePushDto): Promise<number>;
   getRecentPushes(dto: GetRecentPushesDto): Promise<TblPushstsmsg[]>;
   getScheduledPushes(): Promise<PushMaster[]>;
-  getPushStsMsgDetail(idx: string): Promise<TblPushstsmsg | null>;
+  getPushStsMsgDetail(idx: string): Promise<IPushStsMsg | null>;
   updatePushStatus(dto: UpdatePushStatusDto): Promise<UpdatePushStatusDto>;
 }
