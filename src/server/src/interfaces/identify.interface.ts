c37128dd32503acb559b91ed2742abd43efc17ac
sh5080 <prpn97@gmail.com>
2025-01-16 11:41:32 +0900

    fix: sequelize 모델사용

--- src/server/src/interfaces/identify.interface.ts
+++ src/server/src/interfaces/identify.interface.ts
@@ -3,11 +3,12 @@ import {
   GetIdentifiesDto,
   UpdateIdentifyDto,
 } from "@push-manager/shared";
-import { TestIdentify } from "../entities/testIdentify.entity";
+import { TestIdentify } from "../models/TestIdentify";
 
 export interface IIdentifyService {
-  createIdentify(dto: CreateIdentifyDto): Promise<number>;
+  createIdentify(dto: CreateIdentifyDto): Promise<TestIdentify>;
   getIdentifies(dto: GetIdentifiesDto): Promise<TestIdentify[]>;
+  getIdentify(idx: number): Promise<TestIdentify | null>;
   updateIdentify(dto: UpdateIdentifyDto): Promise<TestIdentify>;
-  deleteIdentify(idx: number): Promise<void>;
+  deleteIdentify(idx: number): Promise<number>;
 }
