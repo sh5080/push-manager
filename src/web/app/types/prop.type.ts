51673d16eb9e427580566ad41c005866662f1e72
sh5080 <prpn97@gmail.com>
2025-01-23 14:44:06 +0900

    feat: button type

---
+++ src/web/app/types/prop.type.ts
@@ -0,0 +1,13 @@
+export type ButtonVariant =
+  | "line"
+  | "point"
+  | "red-point"
+  | "green-point"
+  | "solid"
+  | "square-line"
+  | "square-point"
+  | "square-red"
+  | "square-green"
+  | "square-solid";
+
+export type ButtonSize = "32" | "38" | "46";
