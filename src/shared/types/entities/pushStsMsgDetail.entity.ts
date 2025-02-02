f95a3cae9f4ced2a93fb645db090344a6895eb62
sh5080 <prpn97@gmail.com>
2025-02-02 09:35:04 +0900

    feat: detail 반환시 result(pushstssend) 같이 반환

--- src/shared/types/entities/pushStsMsgDetail.entity.ts
+++ src/shared/types/entities/pushStsMsgDetail.entity.ts
@@ -5,3 +5,14 @@ export interface IPushStsMsgDetail {
   deviceType: string;
   appdel: number;
 }
+
+export interface IPushStsMsgResult {
+  idx: string;
+  msgIdx: string;
+  result: string;
+  resultMsg: string;
+  senddate: string;
+  opened: number;
+  deviceType: string;
+  tokenIdx: string;
+}
