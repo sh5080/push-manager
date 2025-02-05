ce68c9eefbf10d4ebc2c35b7f41f51f5cdf8fc93
sh5080 <prpn97@gmail.com>
2025-02-05 13:42:28 +0900

    feat: 발송 상세 분리 및 타입정의

--- src/web/app/types/prop.type.ts
+++ src/web/app/types/prop.type.ts
@@ -12,8 +12,12 @@ export type ButtonVariant =
 
 export type ButtonSize = "32" | "38" | "46";
 
-export type PositionType = "top" | "right" | "bottom" | "left";
+export type PositionType = "top" | "right" | "bottom" | "left" | "center";
 
 export type StatusChipType = "master" | "message";
 
 export type StepEnumType = 0 | 1 | 2 | 3 | 4 | 5;
+
+export type DeviceType = "A" | "I";
+
+export type StatKey = "sent" | "opened" | "failed" | "appdel";
