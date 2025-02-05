8ab6aa7e2460130c86e85608763b47942eb5b40f
sh5080 <prpn97@gmail.com>
2025-02-05 13:41:31 +0900

    feat: isMark 값 통해 옵셔널한 마크처리

--- src/web/app/common/components/infoTooltip.component.tsx
+++ src/web/app/common/components/infoTooltip.component.tsx
@@ -6,25 +6,30 @@ interface InfoTooltipProps {
   content: ReactNode;
   width?: string;
   position?: PositionType;
+  isMark?: boolean;
 }
 
 export function InfoTooltip({
   content,
   width = "w-80",
   position = "top",
+  isMark = true,
 }: InfoTooltipProps) {
   const positionStyles = {
     top: "left-0 bottom-full mb-2",
     right: "left-full top-0 ml-2",
     bottom: "left-0 top-full mt-2",
     left: "right-full top-0 mr-2",
+    center: "left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
   };
 
   return (
     <div className="group relative inline-block">
-      <HiQuestionMarkCircle className="h-4 w-4 text-gray-400 cursor-help" />
+      {isMark && (
+        <HiQuestionMarkCircle className="h-4 w-4 text-gray-400 cursor-help" />
+      )}
       <div
-        className={`invisible group-hover:visible absolute ${positionStyles[position]} ${width} p-3 bg-gray-800 text-white text-xs rounded shadow-lg z-10`}
+        className={`invisible group-hover:visible absolute z-[9999] ${positionStyles[position]} ${width} p-3 bg-gray-800 text-white text-xs rounded shadow-lg`}
       >
         {content}
       </div>
