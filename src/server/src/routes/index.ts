13125908d3ebf514d1a19bf6686023beca1476ca
sh5080 <prpn97@gmail.com>
2025-02-06 12:53:27 +0900

    feat: coupon route

--- src/server/src/routes/index.ts
+++ src/server/src/routes/index.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { pushRoutes } from "./push.route";
 import { identifyRoutes } from "./identify.route";
-// import { couponRoutes } from "./admin/coupon.route";
+import { couponRoutes } from "./admin/coupon.route";
 import { memberRoutes } from "./admin/member.route";
 
 const router = Router();
 
 router.use("/push", pushRoutes);
 router.use("/identify", identifyRoutes);
-// router.use("/admin/coupon", couponRoutes);
+router.use("/admin/coupon", couponRoutes);
 router.use("/admin/member", memberRoutes);
 
 export const apiRoutes = router;
