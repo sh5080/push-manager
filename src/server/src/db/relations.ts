a8d66c6a6dd88e65d558215a7aae0f851acb0a61
sh5080 <prpn97@gmail.com>
2025-03-10 17:44:28 +0900

    feat: pushSendLog, pushSendErrorLog

--- src/server/src/db/relations.ts
+++ src/server/src/db/relations.ts
@@ -1,72 +1,111 @@
 import { relations } from "drizzle-orm/relations";
-import { admin, adminPasswordHist, coupon, couponPool, member, vehicleRegistration, terms, termsAgreement, couponPoolHist, termsAgreementHist } from "./schema";
+import {
+  admin,
+  adminPasswordHist,
+  coupon,
+  couponPool,
+  member,
+  vehicleRegistration,
+  terms,
+  termsAgreement,
+  couponPoolHist,
+  termsAgreementHist,
+  pushSendLog,
+  pushSendErrorLog,
+} from "./schema";
 
-export const adminPasswordHistRelations = relations(adminPasswordHist, ({one}) => ({
-	admin: one(admin, {
-		fields: [adminPasswordHist.adminId],
-		references: [admin.id]
-	}),
+export const adminPasswordHistRelations = relations(
+  adminPasswordHist,
+  ({ one }) => ({
+    admin: one(admin, {
+      fields: [adminPasswordHist.adminId],
+      references: [admin.id],
+    }),
+  })
+);
+
+export const adminRelations = relations(admin, ({ many }) => ({
+  adminPasswordHists: many(adminPasswordHist),
 }));
 
-export const adminRelations = relations(admin, ({many}) => ({
-	adminPasswordHists: many(adminPasswordHist),
+export const couponPoolRelations = relations(couponPool, ({ one, many }) => ({
+  coupon: one(coupon, {
+    fields: [couponPool.couponId],
+    references: [coupon.id],
+  }),
+  member: one(member, {
+    fields: [couponPool.memberId],
+    references: [member.id],
+  }),
+  couponPoolHists: many(couponPoolHist),
 }));
 
-export const couponPoolRelations = relations(couponPool, ({one, many}) => ({
-	coupon: one(coupon, {
-		fields: [couponPool.couponId],
-		references: [coupon.id]
-	}),
-	member: one(member, {
-		fields: [couponPool.memberId],
-		references: [member.id]
-	}),
-	couponPoolHists: many(couponPoolHist),
+export const couponRelations = relations(coupon, ({ many }) => ({
+  couponPools: many(couponPool),
 }));
 
-export const couponRelations = relations(coupon, ({many}) => ({
-	couponPools: many(couponPool),
+export const memberRelations = relations(member, ({ many }) => ({
+  couponPools: many(couponPool),
+  vehicleRegistrations: many(vehicleRegistration),
+  termsAgreements: many(termsAgreement),
 }));
 
-export const memberRelations = relations(member, ({many}) => ({
-	couponPools: many(couponPool),
-	vehicleRegistrations: many(vehicleRegistration),
-	termsAgreements: many(termsAgreement),
+export const vehicleRegistrationRelations = relations(
+  vehicleRegistration,
+  ({ one }) => ({
+    member: one(member, {
+      fields: [vehicleRegistration.memberId],
+      references: [member.id],
+    }),
+  })
+);
+
+export const termsAgreementRelations = relations(
+  termsAgreement,
+  ({ one, many }) => ({
+    term: one(terms, {
+      fields: [termsAgreement.termsId],
+      references: [terms.id],
+    }),
+    member: one(member, {
+      fields: [termsAgreement.memberId],
+      references: [member.id],
+    }),
+    termsAgreementHists: many(termsAgreementHist),
+  })
+);
+
+export const termsRelations = relations(terms, ({ many }) => ({
+  termsAgreements: many(termsAgreement),
 }));
 
-export const vehicleRegistrationRelations = relations(vehicleRegistration, ({one}) => ({
-	member: one(member, {
-		fields: [vehicleRegistration.memberId],
-		references: [member.id]
-	}),
+export const couponPoolHistRelations = relations(couponPoolHist, ({ one }) => ({
+  couponPool: one(couponPool, {
+    fields: [couponPoolHist.couponPoolId],
+    references: [couponPool.id],
+  }),
 }));
 
-export const termsAgreementRelations = relations(termsAgreement, ({one, many}) => ({
-	term: one(terms, {
-		fields: [termsAgreement.termsId],
-		references: [terms.id]
-	}),
-	member: one(member, {
-		fields: [termsAgreement.memberId],
-		references: [member.id]
-	}),
-	termsAgreementHists: many(termsAgreementHist),
+export const termsAgreementHistRelations = relations(
+  termsAgreementHist,
+  ({ one }) => ({
+    termsAgreement: one(termsAgreement, {
+      fields: [termsAgreementHist.termsAgreementId],
+      references: [termsAgreement.id],
+    }),
+  })
+);
+
+export const pushSendLogRelations = relations(pushSendLog, ({ many }) => ({
+  pushSendErrorLogs: many(pushSendErrorLog),
 }));
 
-export const termsRelations = relations(terms, ({many}) => ({
-	termsAgreements: many(termsAgreement),
-}));
-
-export const couponPoolHistRelations = relations(couponPoolHist, ({one}) => ({
-	couponPool: one(couponPool, {
-		fields: [couponPoolHist.couponPoolId],
-		references: [couponPool.id]
-	}),
-}));
-
-export const termsAgreementHistRelations = relations(termsAgreementHist, ({one}) => ({
-	termsAgreement: one(termsAgreement, {
-		fields: [termsAgreementHist.termsAgreementId],
-		references: [termsAgreement.id]
-	}),
-}));
+export const pushSendErrorLogRelations = relations(
+  pushSendErrorLog,
+  ({ one }) => ({
+    pushSendLog: one(pushSendLog, {
+      fields: [pushSendErrorLog.pushSendLogId],
+      references: [pushSendLog.id],
+    }),
+  })
+);
--- src/server/src/db/schema.ts
+++ src/server/src/db/schema.ts
@@ -67,7 +67,7 @@ export const spatialRefSys = pgTable("spatial_ref_sys", {
   authName: varchar("auth_name", { length: 256 }),
   authSrid: integer("auth_srid"),
   srtext: varchar({ length: 2048 }),
-  proj4Text: varchar({ length: 2048 }),
+  proj4text: varchar({ length: 2048 }),
 });
 
 export const location = pgTable(
@@ -438,45 +438,57 @@ export const termsAgreementHist = pgTable(
 
 export const pushSendLog = pgTable("pushSendLog", {
   id: serial().primaryKey(),
+  oneSignalPushId: varchar({ length: 255 }),
   totalCount: integer().notNull(),
   currentCount: integer().notNull().default(0),
+  lastExternalId: varchar({ length: 255 }),
   status: pushSendLogStatus().notNull(),
-  pushId: varchar({ length: 255 }),
-  lastProcessedId: varchar({ length: 255 }),
-  errorMessage: text(),
   createdAt: timestamp({ precision: 3, mode: "string" })
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
   updatedAt: timestamp({ precision: 3, mode: "string" }).notNull(),
 });
 
-// export const geographyColumns = pgView("geography_columns", {
-//   // TODO: failed to parse database type 'name'
-//   fTableCatalog: unknown("f_table_catalog"),
-//   // TODO: failed to parse database type 'name'
-//   fTableSchema: unknown("f_table_schema"),
-//   // TODO: failed to parse database type 'name'
-//   fTableName: unknown("f_table_name"),
-//   // TODO: failed to parse database type 'name'
-//   fGeographyColumn: unknown("f_geography_column"),
-//   coordDimension: integer("coord_dimension"),
-//   srid: integer(),
-//   type: text(),
-// }).as(
-//   sql`SELECT current_database() AS f_table_catalog, n.nspname AS f_table_schema, c.relname AS f_table_name, a.attname AS f_geography_column, postgis_typmod_dims(a.atttypmod) AS coord_dimension, postgis_typmod_srid(a.atttypmod) AS srid, postgis_typmod_type(a.atttypmod) AS type FROM pg_class c, pg_attribute a, pg_type t, pg_namespace n WHERE t.typname = 'geography'::name AND a.attisdropped = false AND a.atttypid = t.oid AND a.attrelid = c.oid AND c.relnamespace = n.oid AND (c.relkind = ANY (ARRAY['r'::"char", 'v'::"char", 'm'::"char", 'f'::"char", 'p'::"char"])) AND NOT pg_is_other_temp_schema(c.relnamespace) AND has_table_privilege(c.oid, 'SELECT'::text)`
-// );
+export const pushSendErrorLog = pgTable(
+  "pushSendErrorLog",
+  {
+    id: serial().primaryKey(),
+    pushSendLogId: integer().notNull(),
+    message: text(),
+    createdAt: timestamp({ precision: 3, mode: "string" })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+  },
+  (table) => [
+    foreignKey({
+      columns: [table.pushSendLogId],
+      foreignColumns: [pushSendLog.id],
+      name: "pushSendErrorLog_pushSendLogId_fkey",
+    }).onDelete("restrict"),
+  ]
+);
 
-// export const geometryColumns = pgView("geometry_columns", {
-//   fTableCatalog: varchar("f_table_catalog", { length: 256 }),
-//   // TODO: failed to parse database type 'name'
-//   fTableSchema: unknown("f_table_schema"),
-//   // TODO: failed to parse database type 'name'
-//   fTableName: unknown("f_table_name"),
-//   // TODO: failed to parse database type 'name'
-//   fGeometryColumn: unknown("f_geometry_column"),
-//   coordDimension: integer("coord_dimension"),
-//   srid: integer(),
-//   type: varchar({ length: 30 }),
-// }).as(
-//   sql`SELECT current_database()::character varying(256) AS f_table_catalog, n.nspname AS f_table_schema, c.relname AS f_table_name, a.attname AS f_geometry_column, COALESCE(postgis_typmod_dims(a.atttypmod), sn.ndims, 2) AS coord_dimension, COALESCE(NULLIF(postgis_typmod_srid(a.atttypmod), 0), sr.srid, 0) AS srid, replace(replace(COALESCE(NULLIF(upper(postgis_typmod_type(a.atttypmod)), 'GEOMETRY'::text), st.type, 'GEOMETRY'::text), 'ZM'::text, ''::text), 'Z'::text, ''::text)::character varying(30) AS type FROM pg_class c JOIN pg_attribute a ON a.attrelid = c.oid AND NOT a.attisdropped JOIN pg_namespace n ON c.relnamespace = n.oid JOIN pg_type t ON a.atttypid = t.oid LEFT JOIN ( SELECT s.connamespace, s.conrelid, s.conkey, replace(split_part(s.consrc, ''''::text, 2), ')'::text, ''::text) AS type FROM ( SELECT pg_constraint.connamespace, pg_constraint.conrelid, pg_constraint.conkey, pg_get_constraintdef(pg_constraint.oid) AS consrc FROM pg_constraint) s WHERE s.consrc ~~* '%geometrytype(% = %'::text) st ON st.connamespace = n.oid AND st.conrelid = c.oid AND (a.attnum = ANY (st.conkey)) LEFT JOIN ( SELECT s.connamespace, s.conrelid, s.conkey, replace(split_part(s.consrc, ' = '::text, 2), ')'::text, ''::text)::integer AS ndims FROM ( SELECT pg_constraint.connamespace, pg_constraint.conrelid, pg_constraint.conkey, pg_get_constraintdef(pg_constraint.oid) AS consrc FROM pg_constraint) s WHERE s.consrc ~~* '%ndims(% = %'::text) sn ON sn.connamespace = n.oid AND sn.conrelid = c.oid AND (a.attnum = ANY (sn.conkey)) LEFT JOIN ( SELECT s.connamespace, s.conrelid, s.conkey, replace(replace(split_part(s.consrc, ' = '::text, 2), ')'::text, ''::text), '('::text, ''::text)::integer AS srid FROM ( SELECT pg_constraint.connamespace, pg_constraint.conrelid, pg_constraint.conkey, pg_get_constraintdef(pg_constraint.oid) AS consrc FROM pg_constraint) s WHERE s.consrc ~~* '%srid(% = %'::text) sr ON sr.connamespace = n.oid AND sr.conrelid = c.oid AND (a.attnum = ANY (sr.conkey)) WHERE (c.relkind = ANY (ARRAY['r'::"char", 'v'::"char", 'm'::"char", 'f'::"char", 'p'::"char"])) AND NOT c.relname = 'raster_columns'::name AND t.typname = 'geometry'::name AND NOT pg_is_other_temp_schema(c.relnamespace) AND has_table_privilege(c.oid, 'SELECT'::text)`
-// );
+// export const geographyColumns = pgView("geography_columns", {	// TODO: failed to parse database type 'name'
+// 	fTableCatalog: unknown("f_table_catalog"),
+// 	// TODO: failed to parse database type 'name'
+// 	fTableSchema: unknown("f_table_schema"),
+// 	// TODO: failed to parse database type 'name'
+// 	fTableName: unknown("f_table_name"),
+// 	// TODO: failed to parse database type 'name'
+// 	fGeographyColumn: unknown("f_geography_column"),
+// 	coordDimension: integer("coord_dimension"),
+// 	srid: integer(),
+// 	type: text(),
+// }).as(sql`SELECT current_database() AS f_table_catalog, n.nspname AS f_table_schema, c.relname AS f_table_name, a.attname AS f_geography_column, postgis_typmod_dims(a.atttypmod) AS coord_dimension, postgis_typmod_srid(a.atttypmod) AS srid, postgis_typmod_type(a.atttypmod) AS type FROM pg_class c, pg_attribute a, pg_type t, pg_namespace n WHERE t.typname = 'geography'::name AND a.attisdropped = false AND a.atttypid = t.oid AND a.attrelid = c.oid AND c.relnamespace = n.oid AND (c.relkind = ANY (ARRAY['r'::"char", 'v'::"char", 'm'::"char", 'f'::"char", 'p'::"char"])) AND NOT pg_is_other_temp_schema(c.relnamespace) AND has_table_privilege(c.oid, 'SELECT'::text)`);
+
+// export const geometryColumns = pgView("geometry_columns", {	fTableCatalog: varchar("f_table_catalog", { length: 256 }),
+// 	// TODO: failed to parse database type 'name'
+// 	fTableSchema: unknown("f_table_schema"),
+// 	// TODO: failed to parse database type 'name'
+// 	fTableName: unknown("f_table_name"),
+// 	// TODO: failed to parse database type 'name'
+// 	fGeometryColumn: unknown("f_geometry_column"),
+// 	coordDimension: integer("coord_dimension"),
+// 	srid: integer(),
+// 	type: varchar({ length: 30 }),
+// }).as(sql`SELECT current_database()::character varying(256) AS f_table_catalog, n.nspname AS f_table_schema, c.relname AS f_table_name, a.attname AS f_geometry_column, COALESCE(postgis_typmod_dims(a.atttypmod), sn.ndims, 2) AS coord_dimension, COALESCE(NULLIF(postgis_typmod_srid(a.atttypmod), 0), sr.srid, 0) AS srid, replace(replace(COALESCE(NULLIF(upper(postgis_typmod_type(a.atttypmod)), 'GEOMETRY'::text), st.type, 'GEOMETRY'::text), 'ZM'::text, ''::text), 'Z'::text, ''::text)::character varying(30) AS type FROM pg_class c JOIN pg_attribute a ON a.attrelid = c.oid AND NOT a.attisdropped JOIN pg_namespace n ON c.relnamespace = n.oid JOIN pg_type t ON a.atttypid = t.oid LEFT JOIN ( SELECT s.connamespace, s.conrelid, s.conkey, replace(split_part(s.consrc, ''''::text, 2), ')'::text, ''::text) AS type FROM ( SELECT pg_constraint.connamespace, pg_constraint.conrelid, pg_constraint.conkey, pg_get_constraintdef(pg_constraint.oid) AS consrc FROM pg_constraint) s WHERE s.consrc ~~* '%geometrytype(% = %'::text) st ON st.connamespace = n.oid AND st.conrelid = c.oid AND (a.attnum = ANY (st.conkey)) LEFT JOIN ( SELECT s.connamespace, s.conrelid, s.conkey, replace(split_part(s.consrc, ' = '::text, 2), ')'::text, ''::text)::integer AS ndims FROM ( SELECT pg_constraint.connamespace, pg_constraint.conrelid, pg_constraint.conkey, pg_get_constraintdef(pg_constraint.oid) AS consrc FROM pg_constraint) s WHERE s.consrc ~~* '%ndims(% = %'::text) sn ON sn.connamespace = n.oid AND sn.conrelid = c.oid AND (a.attnum = ANY (sn.conkey)) LEFT JOIN ( SELECT s.connamespace, s.conrelid, s.conkey, replace(replace(split_part(s.consrc, ' = '::text, 2), ')'::text, ''::text), '('::text, ''::text)::integer AS srid FROM ( SELECT pg_constraint.connamespace, pg_constraint.conrelid, pg_constraint.conkey, pg_get_constraintdef(pg_constraint.oid) AS consrc FROM pg_constraint) s WHERE s.consrc ~~* '%srid(% = %'::text) sr ON sr.connamespace = n.oid AND sr.conrelid = c.oid AND (a.attnum = ANY (sr.conkey)) WHERE (c.relkind = ANY (ARRAY['r'::"char", 'v'::"char", 'm'::"char", 'f'::"char", 'p'::"char"])) AND NOT c.relname = 'raster_columns'::name AND t.typname = 'geometry'::name AND NOT pg_is_other_temp_schema(c.relnamespace) AND has_table_privilege(c.oid, 'SELECT'::text)`);
