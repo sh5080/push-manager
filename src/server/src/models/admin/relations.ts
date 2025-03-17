f231da2d2df950de4f05a932f1fa38adf7e6edfe
sh5080 <prpn97@gmail.com>
2025-03-17 17:57:09 +0900

    feat: member couponpool relation

--- src/server/src/models/admin/relations.ts
+++ src/server/src/models/admin/relations.ts
@@ -1,9 +1,13 @@
 import { CouponPool } from "./CouponPool";
 import { Coupon } from "./Coupon";
-
+import { Member } from "./Member";
 export function initializeAdminRelations() {
   CouponPool.belongsTo(Coupon, {
     foreignKey: "couponId",
+    targetKey: "id",
+  });
+  CouponPool.belongsTo(Member, {
+    foreignKey: "memberId",
     targetKey: "id",
   });
 }
