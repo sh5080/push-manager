3c6de6aa0982a289a50002f080fde6b7ca146f3e
sh5080 <prpn97@gmail.com>
2025-03-31 10:24:39 +0900

    feat: admin repo

--- src/server/src/routes/admin/member.route.ts
+++ src/server/src/routes/admin/member.route.ts
@@ -3,10 +3,11 @@ import { MemberController } from "../../controllers/admin/member.controller";
 import { MemberService } from "../../services/admin/member.service";
 import { MemberRepository } from "../../repositories/admin/member.repository";
 import { NewbestApi } from "../../services/external/newbest.api";
-
+import { AdminRepository } from "../../repositories/admin/admin.repository";
 const router = Router();
 const memberService = new MemberService(
   new MemberRepository(),
+  new AdminRepository(),
   new NewbestApi()
 );
 const memberController = new MemberController(memberService);
