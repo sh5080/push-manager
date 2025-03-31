c8cc0bed51db12199d026c60270d0ea399781611
sh5080 <prpn97@gmail.com>
2025-03-31 10:34:39 +0900

    feat: useSearchParams 사용 컴포넌트 서버 / 클라이언트 경계 설정

--- src/web/app/auth/login/page.tsx
+++ src/web/app/auth/login/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 
+import { Suspense } from "react";
 import { LoginForm } from "./login.component";
+import LoadingOverlay from "@commonComponents/feedback/loading.component";
 
 export default function LoginPage() {
   return (
-    <div className="flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow">
         <h2 className="text-center text-3xl font-extrabold text-gray-900">
           로그인
         </h2>
-        <LoginForm />
+        <Suspense fallback={<LoadingOverlay />}>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   );
