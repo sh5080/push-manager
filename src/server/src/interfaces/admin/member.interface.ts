755104c474661d865e94db59545e09b6d1385ec2
sh5080 <prpn97@gmail.com>
2025-04-02 17:41:17 +0900

    feat: 한영본 조회

--- src/server/src/interfaces/admin/member.interface.ts
+++ src/server/src/interfaces/admin/member.interface.ts
@@ -1,11 +1,20 @@
 import {
   GetMemberDto,
   GetMemberListDto,
+  IMemberGetRes,
   IMemberWithNewbestInfo,
 } from "@push-manager/shared";
 import { admin, member } from "../../db/schema";
 export interface IMemberService {
   getMember(dto: GetMemberDto): Promise<IMemberWithNewbestInfo>;
-  getMemberList(dto: GetMemberListDto): Promise<(typeof member.$inferSelect)[]>;
+  getMemberList(dto: GetMemberListDto): Promise<
+    {
+      memNo: string;
+      createdAt: string;
+    }[]
+  >;
   getAdminByEmail(email: string): Promise<typeof admin.$inferSelect | null>;
+  getMembersAccountInfo(
+    memNoList: string[]
+  ): Promise<{ bestshopNm?: string; address1?: string; address2?: string }[]>;
 }
--- src/server/src/services/admin/member.service.ts
+++ src/server/src/services/admin/member.service.ts
@@ -7,12 +7,13 @@ import { IMemberService } from "../../interfaces/admin/member.interface";
 import { MemberRepository } from "../../repositories/admin/member.repository";
 import { AdminRepository } from "../../repositories/admin/admin.repository";
 import { NewbestApi } from "../external/newbest.api";
-
+import { AccountApi } from "../external/account.api";
 export class MemberService implements IMemberService {
   constructor(
     private readonly memberRepository: MemberRepository,
     private readonly adminRepository: AdminRepository,
-    private readonly newbestApi: NewbestApi
+    private readonly newbestApi: NewbestApi,
+    private readonly accountApi: AccountApi
   ) {}
 
   async getMember(dto: GetMemberDto) {
@@ -23,6 +24,7 @@ export class MemberService implements IMemberService {
     }
 
     const newbestInfo = await this.newbestApi.getMemberInfo(member.ci);
+
     return { ...member, newbestInfo: newbestInfo[0] };
   }
 
@@ -32,5 +34,35 @@ export class MemberService implements IMemberService {
 
   async getAdminByEmail(email: string) {
     return await this.adminRepository.findByEmail(email);
+  }
+  async getMembersAccountInfo(
+    memNoList: string[]
+  ): Promise<{ bestshopNm?: string; address1?: string; address2?: string }[]> {
+    const ciList = await Promise.all(
+      memNoList.map(async (memNo) => {
+        const member = await this.memberRepository.findByDto({ memNo });
+        return { ci: member?.ci };
+      })
+    );
+    const accountInfo = await Promise.all(
+      ciList.map(async ({ ci }) => {
+        try {
+          const res = await this.accountApi.getMemberInfo(ci as string);
+          return {
+            bestshopNm: res.bestshopNm,
+            address1: res.address1,
+            address2: res.address2,
+          };
+        } catch (error) {
+          console.error(`Error fetching account info for CI ${ci}:`, error);
+        }
+      })
+    );
+
+    return accountInfo.map((item) =>
+      item
+        ? item
+        : { bestshopNm: undefined, address1: undefined, address2: undefined }
+    );
   }
 }
