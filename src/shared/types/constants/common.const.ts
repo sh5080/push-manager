8fb96a0a5005570bbf82da1cf21f98280d3d60c2
sh5080 <prpn97@gmail.com>
2025-06-05 09:29:19 +0900

    feat: member 관련 상수 정의

--- src/shared/types/constants/common.const.ts
+++ src/shared/types/constants/common.const.ts
@@ -33,11 +33,27 @@ export const Order = {
 
 export type OrderType = (typeof Order)[keyof typeof Order];
 
-export const memberGrade = {
-  AS05: "AS05",
-  AS04: "AS04",
-  AS03: "AS03",
-  AS02: "AS02",
-  AS01: "AS01",
-  AS00: "AS00",
+export const MemberGrade = {
+  Bronze: "AS05",
+  Silver: "AS04",
+  Gold: "AS03",
+  Platinum: "AS02",
+  Diamond: "AS01",
+  DiamondBlack: "AS00",
 } as const;
+
+export const GradeName = {
+  AS05: "브론즈",
+  AS04: "실버",
+  AS03: "골드",
+  AS02: "플래티넘",
+  AS01: "다이아몬드",
+  AS00: "다이아몬드 블랙",
+} as const;
+
+export const ButtonText = {
+  EXCEL_DOWNLOAD: "엑셀 다운로드",
+  DOWNLOAD_LOADING: "다운로드 중...",
+} as const;
+
+export type ButtonTextType = (typeof ButtonText)[keyof typeof ButtonText];
